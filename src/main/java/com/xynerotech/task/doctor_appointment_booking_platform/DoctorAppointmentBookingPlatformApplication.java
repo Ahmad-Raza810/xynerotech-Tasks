d5f6521dea838f047c61ca5546376f50361fa42b
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-19 16:01:40 +0530

    The main java class.

---
+++ src/main/java/com/xynerotech/task/doctor_appointment_booking_platform/DoctorAppointmentBookingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.xynerotech.task.doctor_appointment_booking_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAppointmentBookingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorAppointmentBookingPlatformApplication.class, args);
+	}
+
+}
