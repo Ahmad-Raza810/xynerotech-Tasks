b09020b23c9a1d844480cb51ee46ff1c7a55ea42
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-19 16:08:57 +0530

    adding resource not found exception..

---
+++ src/main/java/com/xynerotech/task/doctor_appointment_booking_platform/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.xynerotech.task.doctor_appointment_booking_platform.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
