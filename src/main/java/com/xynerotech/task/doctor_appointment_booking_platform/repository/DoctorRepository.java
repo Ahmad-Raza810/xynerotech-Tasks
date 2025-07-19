8ac410ce729a259d497d8ae90463374ddb17ea2a
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-19 16:03:38 +0530

    adding DoctorRepository..

---
+++ src/main/java/com/xynerotech/task/doctor_appointment_booking_platform/repository/DoctorRepository.java
@@ -0,0 +1,8 @@
+package com.xynerotech.task.doctor_appointment_booking_platform.repository;
+
+import com.xynerotech.task.doctor_appointment_booking_platform.entity.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoctorRepository extends JpaRepository<Doctor,Long> {
+
+}
