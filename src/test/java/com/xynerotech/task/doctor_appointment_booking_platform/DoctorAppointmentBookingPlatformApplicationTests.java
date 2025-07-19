5d6f01b4dfcbb5049c92946a31543fca346f5b38
Ahmad Raza <mustafaraza03898@gmail.com>
2025-07-19 16:09:57 +0530

    testing folder.

---
+++ src/test/java/com/xynerotech/task/doctor_appointment_booking_platform/DoctorAppointmentBookingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xynerotech.task.doctor_appointment_booking_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAppointmentBookingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
